f72369aeb17e480b3398af7cf4e685684fcbb1d8
Ayush Dangi <102340150+Ayushselfcoder@users.noreply.github.com>
2023-06-12 20:09:07 +0530

    Removed My Bot Token 😂

--- Settings/config.js
+++ Settings/config.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 module.exports = {
     webhook: {
-        error: "https://discord.com/api/webhooks/1085420191137149008/bcD6fxnxVnMQ8l_yV9oljK2aDxXpyNlG7wQ7xeL0ASzbIdJfJONZZHXXi1AQ-gvVKvAv" || process.env.error, //webhook link where error logs are sent
-        join: "https://discord.com/api/webhooks/1085425141007274054/QKiL2MuqVqC_ZqmzEgdaQHo64mJRVnrA5ZCzy6cH8sbORw8f11lFRqIwifx7wOA1ZrB-" || process.env.join, //webhook link where join logs are sent
-        leave: "https://discord.com/api/webhooks/1085426926488915998/9KKV20TTzian5yOjy_pHVfnLEbUv7mQyUw5mdK_1K3EIQ45BGh39T5KIYbG86PzWpQ2J" || process.env.leave, //webhook link where leave logs are sent
+        error: "" || process.env.error, //webhook link where error logs are sent
+        join: "" || process.env.join, //webhook link where join logs are sent
+        leave: "" || process.env.leave, //webhook link where leave logs are sent
     },
 
-    TOKEN: "OTg4MzU1NDk4MzcxODQyMTA4.Gjd95y.yQWqElusxf_luMFCyn64V670Qlk0m3-5A9632w" || process.env.TOKEN, //Your Bot Token
-    MONGO_URI: "mongodb+srv://nityam:nityam@50th.rbhohex.mongodb.net/?retryWrites=true&w=majority" || process.env.MONGO_URI, //Mongo Uri
+    TOKEN: "" || process.env.TOKEN, //Your Bot Token
+    MONGO_URI: "" || process.env.MONGO_URI, //Mongo Uri
     EMBED_COLOR: "Orange" || process.env.EMBED_COLOR, //Embed Color
     DEV_ID: "1051806381461745664" || process.env.DEV_ID, //Developer ID
     OWNER_ID: "1051806381461745664" || process.env.OWNER_ID, //Owner ID\
@@ -16,8 +16,8 @@ module.exports = {
 
     // Spotify support playlist more 100+ track || false = default || Can get from here: https://developer.spotify.com/dashboard/applications
     SPOTIFY_TRACKS: parseBoolean(process.env.SPOTIFY_TRACKS || true),
-    SPOTIFY_ID: process.env.SPOTIFY_ID || "c594853244e04c22809850ac5515354c",
-    SPOTIFY_SECRET: process.env.SPOTIFY_SECRET ||"bc4929f4f59a4d05a3558bcc2c70c2e4"
+    SPOTIFY_ID: process.env.SPOTIFY_ID || "",
+    SPOTIFY_SECRET: process.env.SPOTIFY_SECRET ||""
 }
 
 function parseBoolean(ask) {
