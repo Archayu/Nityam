7d4c49ee464f301c8068d53b944339f5efbd1e44
Ayushselfcoder <102340150+Ayushselfcoder@users.noreply.github.com>
2023-06-12 20:15:30 +0530

    Beta Purge Commands

--- Handler/Manager.js
+++ Handler/Manager.js
@@ -1,9 +1,16 @@
 const { white, green } = require("chalk");
 
 module.exports = (client) => {
-    require("./Manager/Antinuke.js")(client);
-    require("./Manager/InteractionCreate.js")(client);
-   // require("./Manager/Track.js")(client);
-   // require("./Manager/Player.js")(client);
-    console.log(white('[') + green('INFO') + white('] ') + green('System ') + white('Manager Files') + green(' Loaded!'));
+  require("./Manager/Antinuke.js")(client);
+  require("./Manager/InteractionCreate.js")(client);
+  require("./Manager/Purger.js")(client);
+  // require("./Manager/Player.js")(client);
+  console.log(
+    white("[") +
+      green("INFO") +
+      white("] ") +
+      green("System ") +
+      white("Manager Files") +
+      green(" Loaded!")
+  );
 };
